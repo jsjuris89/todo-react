a9f7dd260cf5c8cee17e67c59a6f33b782677590
jsjuris89 <jsjuris89@gmail.com>
2020-11-08 10:47:16 +0200

    POST register (network) response receives data as an object

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,19 @@ const app = express();
 const cors = require("cors");
 
 app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   console.log(req.url);
   res.send("<h1>Hello</h1>"); //determine the content-type automatically
 });
 app.post("/register", (req, res) => {
-  res.send("Register working");
+  // res.send("Register working");
+  const { username, password } = req.body;
+  res.json({
+    username,
+    password,
+  });
 });
 
 app.listen(4000, (err) => {
